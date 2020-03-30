b603f8c7565d662fb5bae73e381cc09155887fa4
Pc <752952805@qq.com>
2020-03-31 01:31:43 +0800

    PC删除测试

--- basic-code/test/src/cn/itcast/day06/demo02/Test.java
+++ basic-code/test/src/cn/itcast/day06/demo02/Test.java
@@ -5,8 +5,8 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("测试添加git");
-        System.out.println("笔记本测试git");
-        System.out.println("第二次测试添加");
-        System.out.println("笔记本添加");
+
+
+
     }
 }
