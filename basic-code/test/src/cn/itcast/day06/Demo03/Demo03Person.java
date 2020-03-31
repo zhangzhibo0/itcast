ddda4aa4865e9870724a889c134a24abce8f5db4
laptop <752952805@qq.com>
2020-03-31 20:14:05 +0800

    斜船台

---
+++ basic-code/test/src/cn/itcast/day06/Demo03/Demo03Person.java
@@ -0,0 +1,12 @@
+package cn.itcast.day06.Demo03;
+
+public class Demo03Person {
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.name="华为";
+        person.setAge(-20);
+        person.show();
+    }
+}
