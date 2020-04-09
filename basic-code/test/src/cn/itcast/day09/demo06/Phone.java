0e7b89b1b188658946286bf2f291d7c802471dfa
Pc <752952805@qq.com>
2020-04-09 14:16:35 +0800

    PC机

--- basic-code/test/src/cn/itcast/day09/demo06/Phone.java
+++ basic-code/test/src/cn/itcast/day09/demo06/Phone.java
@@ -1,13 +1,15 @@
 package cn.itcast.day09.demo06;
 
 public class Phone {
-    public void  call(){
+    public void call() {
         System.out.println("打电话");
     }
-    public void send(){
+
+    public void send() {
         System.out.println("发短信");
     }
-    public void show(){
+
+    public void show() {
         System.out.println("显示号码");
     }
 }
