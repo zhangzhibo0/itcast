8a15166569890c098a57bc9222163a7f13acbd58
Pc <752952805@qq.com>
2020-04-29 08:22:34 +0800

    pc

--- basic-code/test/src/cn/itcast/day10instanceof/demo08test/Usb.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.day10instanceof.demo08test;
-
-public interface Usb {
-
-    public abstract void open();
-    public abstract void close();
-
-}
--- basic-code/test/src/com/itheima/day01_APIiml/Person.java
+++ basic-code/test/src/com/itheima/day01_APIiml/Person.java
@@ -1,42 +1,30 @@
 package com.itheima.day01_APIiml;
 
+import java.util.Objects;
+
 public class Person {
-    private  String name;
+    private String name;
     private int age;
-    public Person(){
+
+
+    public Person() {
 
     }
-    public Person(String name , int age){
+
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
-    }
 
-    @Override
-    public String toString(){
-        return "Person{name =" +  name + ",age=" + age + "}";
     }
 
 
-    @Override
-    public boolean equals(Object obj){
+    //@toString重写
+//    @Override
+//    public String toString(){
+//        return "Person{name =" +  name + ",age=" + age + "}";
+//    }
 
-        if(obj == this){
-            return true;
-        }
-
-        if( obj == null){
-            return false;
-        }
-
-        if( obj instanceof Person) {
-            Person p = (Person) obj;
-            boolean b = this.equals(obj) && this.age == age;
-            return b;
-        }
-        return false;
-    }
-
-   // 自动生成
+    // 自动生成toString重写方法
 //    @Override
 //    public String toString() {
 //        return "Person{" +
@@ -44,6 +32,44 @@ public class Person {
 //                ", age=" + age +
 //                '}';
 //    }
+
+
+//    //手动重写equals方法
+//    //@Override
+//    public boolean equals(Object obj) {
+//
+//        if(this == obj){
+//
+//            return true;
+//        }
+//
+//        if( obj == null){
+//            return false;
+//        }
+//
+//
+//        if(obj instanceof Person){
+//            Person p = (Person) obj;
+//            boolean b = this.name.equals(p.name) && this.age == p.age;
+//            return b;
+//        }
+//
+//        return false;
+//    }
+
+
+    //自动生成
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(name, person.name);
+    }
+
+
+
 
     public String getName() {
         return name;
