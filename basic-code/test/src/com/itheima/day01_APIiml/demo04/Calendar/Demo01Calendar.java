308a76fac0cb9da7da13ffc8b44050a471bbd776
laptop <752952805@qq.com>
2020-04-30 13:58:54 +0800

    斜船台

---
+++ basic-code/test/src/com/itheima/day01_APIiml/demo04/Calendar/Demo01Calendar.java
@@ -0,0 +1,12 @@
+package com.itheima.day01_APIiml.demo04.Calendar;
+
+import java.util.Calendar;
+
+public class Demo01Calendar {
+    public static void main(String[] args) {
+
+        Calendar c = Calendar.getInstance();
+        System.out.println(c);
+
+    }
+}
