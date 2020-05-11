10ac8c2c1d90a9368d4438fabecd1e22aea5c3c9
Pc <752952805@qq.com>
2020-05-11 14:03:41 +0800

    pc

---
+++ basic-code/test/src/com/itheima/day02Collection/Demo02Iterator.java
@@ -0,0 +1,35 @@
+package com.itheima.day02Collection;
+
+
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Demo02Iterator {
+    public static void main(String[] args) {
+        //创建一个集合对象
+        Collection<String> coll = new ArrayList<>();
+
+        coll.add("姚明");
+        coll.add("科比");
+        coll.add("詹姆斯");
+        coll.add("安佛森");
+        coll.add("赵兴");
+
+        //接口 等于 实现类，就是多态
+        Iterator<String> it = coll.iterator();
+        //使用Iterator方法判断 和 取出数据.
+        while(it.hasNext()){
+            String e = it.next();
+            System.out.println(e);
+        }
+        System.out.println("============");
+
+        //使用for循环
+        for(Iterator<String> it2 = coll.iterator(); it2.hasNext(); ){
+            String e = it2.next();
+            System.out.println("for循环:  " + e);
+        }
+
+    }
+}
---
+++ basic-code/test/src/com/itheima/day02Collection/Demo03foreach.java
@@ -0,0 +1,33 @@
+package com.itheima.day02Collection;
+
+import java.util.ArrayList;
+
+public class Demo03foreach {
+    public static void main(String[] args) {
+
+        //集合
+        Demo01();
+        //数组
+        Demo02();
+    }
+
+    private static void Demo01(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+        list.add("ddd");
+        System.out.println("集合:");
+        for(String li : list){
+            System.out.println(li);
+        }
+    }
+    private static void Demo02(){
+        int [] arr = {1,2,3,4,5};
+        System.out.println("数组:");
+        for(int ar: arr){
+            System.out.println(ar);
+        }
+    }
+}
+
