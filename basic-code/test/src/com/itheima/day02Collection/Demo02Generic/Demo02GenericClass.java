4d9dd009c4b0f8b6cda30b986e11840dfaf81db0
Pc <752952805@qq.com>
2020-05-11 15:55:34 +0800

    pc

---
+++ basic-code/test/src/com/itheima/day02Collection/Demo02Generic/Demo02GenericClass.java
@@ -0,0 +1,22 @@
+package com.itheima.day02Collection.Demo02Generic;
+
+public class Demo02GenericClass {
+    public static void main(String[] args) {
+
+        GenericClass gc = new  GenericClass();
+        gc.setName("只能是字符串");
+        Object obj = gc.getName();
+        System.out.println(obj);
+
+
+        GenericClass<Integer> gc2= new GenericClass<>();
+        gc2.setName(1);
+        Integer name = gc2.getName();
+        System.out.println(name);
+
+        GenericClass<String> gc3 = new GenericClass<>();
+        gc3.setName("字符串字符串");
+        String name1 = gc3.getName();
+        System.out.println(name1);
+    }
+}
---
+++ basic-code/test/src/com/itheima/day02Collection/Demo02Generic/GenericClass.java
@@ -0,0 +1,16 @@
+package com.itheima.day02Collection.Demo02Generic;
+
+public class GenericClass<E> {
+    private E name;
+
+    public E getName() {
+        return name;
+    }
+
+    public void setName(E name) {
+        this.name = name;
+    }
+
+
+
+}
