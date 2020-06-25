04ef113399ec98668d07944d412ed45a5f49958a
Pc <752952805@qq.com>
2020-06-25 18:37:16 +0800

    pc

--- basic-code/test/src/com/itheima/day04Map/demo01Map/Demo01Map.java
+++ basic-code/test/src/com/itheima/day04Map/demo01Map/Demo01Map.java
@@ -21,6 +21,25 @@ package com.itheima.day04Map.demo01Map;
 
  */
 
+/*
+      1.  public V put(K key, V value); 把指定的键与指定的值添加到Map集合中。
+            返回值:v
+                存储键值对的时候，key不重复,返回值V是null
+                存储键值对的时候，key重复,会使用心得value替换map中的重复的value,返回被替换的value值
+
+       2.public V remove(Object key); 把指定的键 所对应的键值对 元素 在Map集合中删除，返回被删除元素的值.
+            返回值:v
+                key存在，v返回被删除的值
+                key不存在，v返回null
+       3.public V get(Object key) 根据指定键，在Map集合中获取对应的值.
+            返回值:
+                key存在,返回对应的value值
+                key不存在,返回null
+       4.boolean containsKey(Object key)判断结合中是否包含指定的键.
+            包含返回true,不包含返回false
+
+ */
+
 import java.util.Map;
 import java.util.HashMap;
 
