7c52ba4bdf8024d66eddce6f298fb820b477e97b
Pc <752952805@qq.com>
2020-07-06 11:55:46 +0800

    pc

--- basic-code/test/src/com/itheima/day05Throwable/demo06throwable/Demo01Throwable.java
+++ basic-code/test/src/com/itheima/day05Throwable/demo06throwable/Demo01Throwable.java
@@ -1,5 +1,7 @@
 package com.itheima.day05Throwable.demo06throwable;
 
+import java.io.IOException;
+
 /*
     throwable类 提供了3个方法
      String getMessage() 返回此throwable简短描述
@@ -8,7 +10,27 @@ package com.itheima.day05Throwable.demo06throwable;
  */
 public class Demo01Throwable {
     public static void main(String[] args) {
+        try{
+            readFile("a.tet");
+        }catch(Exception e){
 
+            //System.out.println("文件后缀名不正确");
+
+            //String getMessage() 返回此throwable简短描述
+            System.out.println(e.getMessage());
+            //String toString() 返回throwable详细详细
+            System.out.println(e.toString());
+            //void pringStackTrace()jvm打印异常对象 默认方法，异常信息
+            e.printStackTrace();
+
+        }
+        System.out.println("后续代码");
+    }
+    public static void readFile(String fileName) throws IOException{
+
+        if(!fileName.endsWith(".txt")){
+            throw new  IOException("文件后缀名不正确");
+        }
     }
 
 }
